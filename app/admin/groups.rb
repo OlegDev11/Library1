399c520445ccd5d40cdff8b81d1df5a7c04b259d
Oleg Pavlenko <mr.mastercredit@gmail.com>
2019-03-13 19:10:14 +0200

    add show to group

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Group do
   permit_params :name
   actions :all
+
+    show do
+       attributes_table do
+          table_for group.books do
+             column "Books" do |book|
+             link_to book.name, [:admin, book]
+             end
+          end
+       end
+    end
 end
