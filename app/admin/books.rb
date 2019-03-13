a738ae76b4ff0ac483ab1adade5a700600594a51
Oleg Pavlenko <mr.mastercredit@gmail.com>
2019-03-13 13:44:18 +0200

    add check box in Active Admin

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,4 +1,35 @@
 ActiveAdmin.register Book do
   actions :all
-  permit_params :name, :description, :group_id, :avatar 
+  permit_params :name, :description, :avatar, group_ids: [], author_ids: []
+
+  show do
+  attributes_table do
+    table_for book.authors do
+     column "Authors" do |author|
+       link_to author.name, [:admin, author]
+     end
+
+   end
+    row :description
+    row :name
+    row :avatar  do |book|
+        image_tag book.avatar.url
+      end
+    table_for book.groups do
+     column "Groups" do |group|
+       link_to group.name, [:admin, group]
+     end
+   end
+  end
+end
+
+  form do |f|
+      f.inputs "Add/Edit book" do
+      f.input :name
+      f.input :authors, :as => :check_boxes
+      f.input :description
+      f.input :groups, :as => :check_boxes
+    end
+     actions
+  end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,9 @@
 class Author < ApplicationRecord
    has_and_belongs_to_many :books
 
-   validates :last_name, :first_name,  presence: true 
+   validates :last_name, :first_name,  presence: true
+
+   def name
+      "#{first_name} #{last_name}" 
+   end
 end
