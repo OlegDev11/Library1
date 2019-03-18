82d41757129a4b0c8541ce158be5bf4e751b2cc9
Oleg Pavlenko <mr.mastercredit@gmail.com>
2019-03-18 20:34:10 +0200

    config carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
         :provider              => 'AWS',
-        :aws_access_key_id     => ENV['AKIAJDOUCEXFPLQHLGOA'],
-        :aws_secret_access_key => ENV['J3jzxmoj2 / yNMSwxwKp1uOg5e + H6cqD / К + o8mORW'],
-        :region                => ENV['US East(N.Virginia)']
+        :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region                => ENV['AWS_REGION']
     }
 
     config.storage = :fog
-    config.fog_directory = ENV['library1777']
+    config.fog_directory = ENV['AWS_S3_BUCKET']
   end
 end
