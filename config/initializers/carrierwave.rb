6903f39fb16c46bb30280ead93a2b7604f333873
Oleg Pavlenko <mr.mastercredit@gmail.com>
2019-03-20 19:57:55 +0200

    ee

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,7 @@
+require 'carrierwave/storage/fog'
 if  Rails.env.production?
    CarrierWave.configure do |config|
-    #   config.fog_provider = 'fog/aws'
+       config.fog_provider = 'fog/aws'
       config.fog_credentials = {
         :provider => 'AWS',
         :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
@@ -8,5 +9,6 @@ if  Rails.env.production?
         :region => ENV['AWS_S3_REGION']
       }
       config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
+      config.storage = :fog
    end
 end
