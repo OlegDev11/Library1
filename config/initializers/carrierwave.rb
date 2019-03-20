feaa0f9e370d246b2610043f1cd89756d95d44dd
Oleg Pavlenko <mr.mastercredit@gmail.com>
2019-03-20 19:54:33 +0200

    ee

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,12 @@
-require 'carrierwave/storage/fog'
-if  Rails.env.production? || Rails.env.development?
-  CarrierWave.configure do |config|
-    config.storage = :file
-  end
-else
-  CarrierWave.configure do |config|
-      config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => ENV['AWS_S3_REGION']
-    }
-    config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
-    config.asset_host = "#{ENV['AWS_S3_ASSET_URL']}/#{ENV['AWS_S3_BUCKET_NAME']}"
-    config.storage = :fog
-  end
+if  Rails.env.production?
+   CarrierWave.configure do |config|
+    #   config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+        :provider => 'AWS',
+        :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region => ENV['AWS_S3_REGION']
+      }
+      config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
+   end
 end
